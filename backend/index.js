40335aba37677ae296b125010cf629e9289985dc
Mohd. Shayan <70843731+MohdShayan@users.noreply.github.com>
2024-09-29 01:33:22 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/movies", movieRoutes);
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   ConnectDB();
