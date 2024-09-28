166e9c0d1141d9000511501f211dcebd4ee4877a
Mohd. Shayan <70843731+MohdShayan@users.noreply.github.com>
2024-09-29 01:23:14 +0530

    Update connectDB.js

--- backend/Connection/connectDB.js
+++ backend/Connection/connectDB.js
@@ -1,12 +1,37 @@
-import mongoose from "mongoose"
+// import mongoose from "mongoose"
 
 
-export const ConnectDB = async() => {
-    try {
-        const conn= await mongoose.connect(process.env.MONGO_URI)
-        console.log(`MongoDB Connected ${conn.connection.host}`)
-    } catch (error) {
-        console.error(`Error : ${error.message}`);
-        process.exit(1);
-    }
-}
+// export const ConnectDB = async() => {
+//     try {
+//         const conn= await mongoose.connect(process.env.MONGO_URI)
+//         console.log(`MongoDB Connected ${conn.connection.host}`)
+//     } catch (error) {
+//         console.error(`Error : ${error.message}`);
+//         process.exit(1);
+//     }
+// }
+
+import mongoose from "mongoose";
+
+// Store the database connection state
+let isConnected = false;
+
+export const ConnectDB = async () => {
+  if (isConnected) {
+    console.log("=> Using existing database connection");
+    return;
+  }
+
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    
+    isConnected = conn.connections[0].readyState;
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1); // Exit process with failure
+  }
+};
